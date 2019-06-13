dde77e03e6ee7e5679ca2ed122a22763e35aa227
Fabio <fabrix.xm@gmail.com>
2019-06-13 15:42:36 +0200

    Use mailparser to parse incoming mail

--- matrix-send-queue-mail.py
+++ matrix-send-queue-mail.py
@@ -3,20 +3,24 @@ import io
 import os
 import sys
 import datetime
+import mailparser
 from config import config
+
 
 
 filename = str(datetime.datetime.now()).replace(":","-").replace(" ","_")
 filepath = os.path.join(config['queue_path'], filename)
 
-input_stream = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
+# input_stream = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
 
-inheader=True
+message = mailparser.parse_from_file_obj(sys.stdin)
+
+mfrom = ", ".join([" ".join(r) for r in message.from_])
+msubj = message.subject
+mbody = "\n\n".join(message.text_plain)
+
 with open(filepath, "w", encoding="UTF-8") as fout:
-    for l in input_stream:
-        if not inheader:
-            fout.write(l)
-        if inheader and (l.startswith("From:") or l.startswith("Subject:")):
-            fout.write(l)
-        if inheader and l.strip()=="":
-            inheader = False
+    fout.write("From: {}\n".format(mfrom))
+    fout.write(msubj  + "\n\n")
+    fout.write(mbody)
+
